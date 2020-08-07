984738c9db6468c20943c7ec094e3594ef599d59
Elliot Young <efonov@10-197-137-119.sds.oregonstate.edu>
2020-08-07 13:27:10 -0400

    replace

--- createDependent.php
+++ createDependent.php
@@ -1,84 +1,89 @@
 <?php
-// Include config file
-require_once "config.php";
+	session_start();
+	ob_start();
+	$Ssn = $_SESSION["Ssn"];
+	// Include config file
+	require_once "config.php";
+?>
+
+
+<?php 
+	// Define variables and initialize with empty values
+	$Dependent_name = $Sex = $Bdate = $Relationship = "";
+	$Ssn_err = $Dependent_name_err = $Sex_err = $Bdate_err = $Relationship_err = "" ;
  
-// Define variables and initialize with empty values
-$Dependent_name = $Sex = $Bdate = $Relationship = "";
-$Dependent_name_err = $Sex_err = $Bdate_err = $Relationship_err = "" ;
- 
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    // Validate Dependent Name
-    $Dependent_name = trim($_POST["Dependent_name"]);
-    if(empty($Dependent_name)){
-        $Dependent_name_err = "Please enter a Dependent Name.";
-    } elseif(!filter_var($Fname, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
-        $Dependent_name_err = "Please enter a valid Dependent Name.";
-    } 
-	// Validate Sex
-    $Sex = trim($_POST["Sex"]);
-    if(empty($Sex)){
-        $Sex_err = "Please enter Sex.";     
-    }
-    // Validate Birthdate
-    $Bdate = trim($_POST["Bdate"]);
-    if(empty($Bdate)){
-        $Bdate_err = "Please enter birthdate.";     
-    }	
-    // Validate Relationship
-    $Relationship = trim($_POST["Relationship"]);
-    if(empty($Relationship)){
-        $Relationship_err = "Please enter a Relationship.";
-    } elseif(!filter_var($Fname, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
-        $Relationship_err = "Please enter a valid Relationship";
-    } 
+	// Processing form data when form is submitted
+	if($_SERVER["REQUEST_METHOD"] == "POST"){
+		// Validate Dependent Name
+		$Dependent_name = trim($_POST["Dependent_name"]);
+		if(empty($Dependent_name)){
+			$Dependent_name_err = "Please enter a Dependent Name.";
+		} elseif(!filter_var($Fname, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+			$Dependent_name_err = "Please enter a valid Dependent Name.";
+		} 
+		// Validate Sex
+		$Sex = trim($_POST["Sex"]);
+		if(empty($Sex)){
+			$Sex_err = "Please enter Sex.";     
+		}
+		// Validate Birthdate
+		$Bdate = trim($_POST["Bdate"]);
+		if(empty($Bdate)){
+			$Bdate_err = "Please enter birthdate.";     
+		}	
+		// Validate Relationship
+		$Relationship = trim($_POST["Relationship"]);
+		if(empty($Relationship)){
+			$Relationship_err = "Please enter a Relationship.";
+		} elseif(!filter_var($Fname, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+			$Relationship_err = "Please enter a valid Relationship";
+		} 
+		// Validate the SSN
+		if(empty($Ssn)){
+			$Ssn_err = "No SSN.";     
+		}
     // Check input errors before inserting in database
-    if(empty($Ssn_err) && empty($Lname_err) && empty($Salary_err) 
-				&& empty($Dno_err)&& empty($Address_err) && empty($Sex_err)){
+		if(empty($Ssn_err) && empty($Dependent_name_err) && empty($Sex_err) && empty($Bdate_err) && empty($Relationship_err) ){
         // Prepare an insert statement
-        $sql = "INSERT INTO DEPENDENT (Ssn, Fname, Lname, Address, Salary, Sex, Bdate, Dno) 
-		        VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-         
-        if($stmt = mysqli_prepare($link, $sql)){
+			$sql = "INSERT INTO DEPENDENT (Essn, Dependent_name, Sex, Bdate, Relationship) VALUES (?, ?, ?, ?, ?)";
+        	if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "isssdssi", $param_Ssn, $param_Fname, $param_Lname, 
-				$param_Address, $param_Salary, $param_Sex, $param_Bdate, $param_Dno);
+				mysqli_stmt_bind_param($stmt, 'sii', $param_Ssn, $$param_Dependent_name, $param_Sex, $param_Bdate, $param_Relationship);
             
-            // Set parameters
-			$param_Ssn = $Ssn;
-            $param_Lname = $Lname;
-			$param_Fname = $Fname;
-			$param_Address = $Address;
-			$param_Sex = $Sex;
-			$param_Bdate = $Bdate;
-            $param_Salary = $Salary;
-            $param_Dno = $Dno;
-            
+				// Set parameters
+				$param_Ssn = $Ssn;
+				$param_Dependent_name = $Dependent_name;
+				$param_Sex = $Sex;
+				$param_Bdate = $Bdate;
+				$param_Relationship = $Relationship;
+        
             // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Records created successfully. Redirect to landing page
-				    header("location: index.php");
-					exit();
-            } else{
-                echo "<center><h4>Error while creating new employee</h4></center>";
-				$Ssn_err = "Enter a unique Ssn.";
-            }
-        }
+				if(mysqli_stmt_execute($stmt)){
+               // Records created successfully. Redirect to landing page
+				//    header("location: index.php");
+				//	exit();
+				} else{
+					// Error
+					
+					$SQL_err = mysqli_error($link);
+				}
+
+			}
          
         // Close statement
         mysqli_stmt_close($stmt);
-    }
-
-    // Close connection
-    mysqli_close($link);
+		
+	}   
+		// Close connection
+		mysqli_close($link);
 }
 ?>
- 
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Create Record</title>
+    <title>Create Dependent</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
         .wrapper{
@@ -93,58 +98,58 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <div class="row">
                 <div class="col-md-12">
                     <div class="page-header">
-                        <h2>Create Record</h2>
+                        <h3>Add a Dependent for SSN = 
+							<?php echo $Ssn;?>			
+						</h3>
                     </div>
-                    <p>Please fill this form and submit to add an Employee record to the database.</p>
-                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-						<div class="form-group <?php echo (!empty($Ssn_err)) ? 'has-error' : ''; ?>">
-                            <label>SSN</label>
-                            <input type="text" name="Ssn" class="form-control" value="<?php echo $Ssn; ?>">
-                            <span class="help-block"><?php echo $Ssn_err;?></span>
-                        </div>
-                 
-						<div class="form-group <?php echo (!empty($Fname_err)) ? 'has-error' : ''; ?>">
-                            <label>First Name</label>
-                            <input type="text" name="Fname" class="form-control" value="<?php echo $Fname; ?>">
-                            <span class="help-block"><?php echo $Fname_err;?></span>
-                        </div>
-						<div class="form-group <?php echo (!empty($Lname_err)) ? 'has-error' : ''; ?>">
-                            <label>Last Name</label>
-                            <input type="text" name="Lname" class="form-control" value="<?php echo $Lname; ?>">
-                            <span class="help-block"><?php echo $Lname_err;?></span>
-                        </div>
-						<div class="form-group <?php echo (!empty($Address_err)) ? 'has-error' : ''; ?>">
-                            <label>Address</label>
-                            <input type="text" name="Address" class="form-control" value="<?php echo $Address; ?>">
-                            <span class="help-block"><?php echo $Address_err;?></span>
-                        </div>
-                        <div class="form-group <?php echo (!empty($Salary_err)) ? 'has-error' : ''; ?>">
-                            <label>Salary</label>
-                            <input type="text" name="Salary" class="form-control" value="<?php echo $Salary; ?>">
-                            <span class="help-block"><?php echo $Salary_err;?></span>
-                        </div>
-						<div class="form-group <?php echo (!empty($Sex_err)) ? 'has-error' : ''; ?>">
-                            <label>Sex</label>
-                            <input type="text" name="Sex" class="form-control" value="<?php echo $Sex; ?>">
-                            <span class="help-block"><?php echo $Sex_err;?></span>
-                        </div>
-						                  
-						<div class="form-group <?php echo (!empty($Birth_err)) ? 'has-error' : ''; ?>">
-                            <label>Birth date</label>
-                            <input type="date" name="Bdate" class="form-control" value="<?php echo date('Y-m-d'); ?>">
-                            <span class="help-block"><?php echo $Birth_err;?></span>
-                        </div>
-                        <div class="form-group <?php echo (!empty($Dno_err)) ? 'has-error' : ''; ?>">
-                            <label>Dno</label>
-                            <input type="number" min ="1" max ="20" name="Dno" class="form-control" value="<?php echo $Dno; ?>">
-                            <span class="help-block"><?php echo $Dno_err;?></span>
-                        </div>
-                        <input type="submit" class="btn btn-primary" value="Submit">
-                        <a href="viewDependents.php" class="btn btn-default">Cancel</a>
-                    </form>
-                </div>
-            </div>        
-        </div>
-    </div>
+				
+<?php
+	echo $SQL_err;		
+	$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+	if (!$conn) {
+		die('Could not connect: ' . mysqli_error());
+	}
+	$sql = "SELECT Pnumber, Pname FROM PROJECT";
+	$result = mysqli_query($conn, $sql);
+	if (!$result) {
+		die("Query to show fields from table failed");
+	}
+	$num_row = mysqli_num_rows($result);	
+?>	
+
+	<form action="<?php echo htmlspecialchars(basename($_SERVER['REQUEST_URI'])); ?>" method="post">
+		<div class="form-group <?php echo (!empty($Ssn_err)) ? 'has-error' : ''; ?>">
+            <label>Project number & name</label>
+			<select name="Pno" class="form-control">
+			<?php
+
+				for($i=0; $i<$num_row; $i++) {
+					$Pnos=mysqli_fetch_row($result);
+					echo "<option value='$Pnos[0]' >".$Pnos[0]."  ".$Pnos[1]."</option>";
+				}
+			?>
+			</select>	
+            <span class="help-block"><?php echo $Pno_err;?></span>
+		</div>
+		<div class="form-group <?php echo (!empty($Hours_err)) ? 'has-error' : ''; ?>">
+			<label>Hours </label>
+			<input type="number" name="Hours" class="form-control" min="1" max="80" value="">
+			<span class="help-block"><?php echo $Hours_err;?></span>
+		</div>
+		<div>
+			<input type="submit" class="btn btn-success pull-left" value="Add Project">	
+			&nbsp;
+			<a href="viewDependents.php" class="btn btn-primary">List Dependents</a>
+
+		</div>
+	</form>
+<?php		
+	mysqli_free_result($result);
+	mysqli_close($conn);
+?>
 </body>
+
 </html>
+
+	
+
