1666fdd043e09cd5e002803c45de6c713c0fb51b
Elliot Young <efonov@10-197-137-119.sds.oregonstate.edu>
2020-08-07 12:57:25 -0400

    Update viewProjects.php

--- viewProjects.php
+++ viewProjects.php
@@ -80,7 +80,10 @@ if(isset($_SESSION["Ssn"]) ){
                         echo "<td>" . $row['Pnumber'] . "</td>";
                         echo "<td>" . $row['Pname'] . "</td>";
                         echo "<td>" . $row['Hours'] . "</td>";
-    
+                        echo "<td>";
+                        echo "<a href='updateEmployee.php?Ssn=". $row['Ssn'] ."' title='Update Record' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span></a>";
+                        echo "<a href='deleteEmployee.php?Ssn=". $row['Ssn'] ."' title='Delete Record' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span></a>";
+                    echo "</td>";
                         echo "</tr>";
                     }
                     echo "</tbody>";                            
